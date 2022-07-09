8667ead7c1b6a2854dacb7c42c5842da6907bb96
Watchitgo333 <bruskyeric@gmail.com>
2022-07-09 14:40:27 -0500

    Added first_jsx

---
+++ assignments/week1/FirstJSX/first_jsx/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Dojo!</h1>
+      <div>
+        <h3>Things I need to do:</h3>
+        <div>
+          <ul>
+            <li>Learn React</li>
+            <li>Feed my cats and dogs</li>
+            <li>Play guitar</li>
+            <li>Eat dinner</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
