b24e3d907cd8b31bad12c1f95e99bbf26235a5a8
Watchitgo333 <bruskyeric@gmail.com>
2022-07-11 20:03:45 -0500

    Added useState and bootstrap

--- assignments/week1/PropItUp/prop_it_up/src/components/PersonCard.js
+++ assignments/week1/PropItUp/prop_it_up/src/components/PersonCard.js
@@ -1,13 +1,20 @@
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
 const PersonCard = (props) => {
     const {firstName, lastName, age, hairColor} = props
+    const [ getAge, setAge ] = useState( parseInt(age) )
     return(
         <div>
             <Card className="col-4">
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {getAge}</p>
                 <p>Hair Color: {hairColor}</p>
+                <Button className="btn btn-primary"
+                    onClick={ ()=> setAge( getAge + 1 ) }>
+                    Birthday Button for {firstName} {lastName}
+                </Button>
             </Card>
         </div>
     );
